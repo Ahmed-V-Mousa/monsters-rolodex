78f3bfa632b28975b6411d076a06b8db32fe2b24
ahmed.v.mousa <ahmed.v.mousa@gmail.com>
2022-01-31 20:37:54 +0200

    Updates

---
+++ precache-manifest.83300c8bd01a59cf08f34b6aa06f6110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae0b8f528a5cb2ae0f13bf94dfcb2f5",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "8440f6a46971dd9c091b",
+    "url": "/monsters-rolodex-complete/static/css/main.ecd9ec5c.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8440f6a46971dd9c091b",
+    "url": "/monsters-rolodex-complete/static/js/main.96396b4c.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
